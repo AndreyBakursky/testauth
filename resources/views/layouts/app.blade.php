bfd7f87cff1f341b6990916751e8ec9148efc837
AndreyBakursky <68955115+AndreyBakursky@users.noreply.github.com>
2020-12-06 23:08:45 +0400

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>@yield('title-block')</title>
         <!-- Fonts -->
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
         <!-- Styles -->
