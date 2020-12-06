cd1b85a277a938fe5e9fda27064a3da6711e9640
AndreyBakursky <68955115+AndreyBakursky@users.noreply.github.com>
2020-12-06 23:08:47 +0400

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+    @section('title-block')Домашняя@endsection
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Домашняя страница') }}
