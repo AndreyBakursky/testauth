956b6d53aa5722caad4d8703a8067666539882b6
AndreyBakursky <68955115+AndreyBakursky@users.noreply.github.com>
2020-12-06 19:40:24 +0400

    Update footer.blade.php

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="pt-4 my-md-5 pt-md-5 mt-5">
+<footer class="pt-4 my-md-5 pt-md-5 mt-5 fixed-bottom">
     <div class="row">
         <div class="col-12 col-md">
             <img class="mb-2" src="/docs/4.5/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
